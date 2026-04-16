78d7b9e479d9c466784c6b041388cf1212570cc8
Maxim-Coder-arch <maximnovikov2206@gmail.com>
2026-04-16 13:23:06 +0300

    add pagination

--- app/components/allGoods/index.tsx
+++ app/components/allGoods/index.tsx
@@ -1,5 +1,4 @@
 // app/catalog/page.tsx (пример использования)
-import { getAllProducts } from '@/lib/get-data/products';
 import { websiteData as data } from '@/data/websiteData';
 import AllGoodsClient from './allGoodsClient';
 
